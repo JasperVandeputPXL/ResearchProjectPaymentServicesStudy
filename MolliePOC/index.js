54eaa2db5331afe1a3ae27dd40777a87ef3a185f
KaiVerleysenPXL <kai.verleysen@student.pxl.be>
2025-05-10 00:15:15 +0200

    poc unification

---
+++ MolliePOC/index.js
@@ -0,0 +1,51 @@
+// een bestelling zoals ze van de java-backend zou komen als DTO:
+const order = {
+    id: 1,
+    lastName: "Familienaam",
+    firstName: "Voornaam",
+    reservationTime: "18:30",
+    email: "mail.adres@gmail.com",
+    comments: "",
+    dishes: [{id: 1, name: "Mosselen met frietjes", amount: 4, unitPrice: 20}, 
+             {id: 2, name: "Videe met frietjes", amount: 2, unitPrice: 15}, 
+             {id: 3, name: "Frikandel met frietjes", amount: 1, unitPrice: 12}],
+    orderstatus: "PLACED",
+    amountPaid: 0,
+}
+
+// expressJS opzet
+import express from 'express';
+const app = express();
+const port = 4001;
+app.use(express.json());
+
+// mollie client opzetten
+import { createMollieClient } from '@mollie/api-client';
+const mollieClient = createMollieClient({ apiKey: 'test_uSTyyjBGrxeJUkaDx4dFzGdzr6kuGA' });
+
+// payment object aanmaken:
+function createPaymentObject(placedOrder) {
+  return {
+    amount: { 
+      value: placedOrder.dishes.map(dish => dish.amount * dish.unitPrice).reduce((sum, lineCost) => sum + lineCost).toFixed(2),
+      currency: 'EUR'
+    },
+    description: placedOrder.lastName,
+    redirectUrl: "http://localhost:80",
+    lines: placedOrder.dishes.map(dish => {
+      return {
+        description: dish.name,
+        unitPrice: { currency: 'EUR', value: dish.unitPrice.toFixed(2) },
+        quantity: dish.amount,
+        totalAmount: { currency: 'EUR', value: (dish.amount * dish.unitPrice).toFixed(2) }
+      }
+    })
+  }
+}
+
+app.get('/payment/start', async (req, res) => {
+    const payment = await mollieClient.payments.create(createPaymentObject(order));
+    res.redirect(payment.getCheckoutUrl()).send();
+});
+
+app.listen(port);
--- StripePOC/server.js
+++ StripePOC/server.js
@@ -1,49 +1,57 @@
-require('dotenv').config();
-const express = require('express');
+// een bestelling zoals ze van de java-backend zou komen als DTO:
+const order = {
+    id: 1,
+    lastName: "Familienaam",
+    firstName: "Voornaam",
+    reservationTime: "18:30",
+    email: "mail.adres@gmail.com",
+    comments: "",
+    dishes: [{id: 1, name: "Mosselen met frietjes", amount: 4, unitPrice: 20}, 
+             {id: 2, name: "Videe met frietjes", amount: 2, unitPrice: 15}, 
+             {id: 3, name: "Frikandel met frietjes", amount: 1, unitPrice: 12}],
+    orderstatus: "PLACED",
+    amountPaid: 0,
+}
+
+// expressJS opzet
+import express from 'express'
 const app = express();
+const port = 3000;
 app.use(express.json());
 app.use(express.static('public'));
 
-// TODO: Set private key into .env file
-// const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
-const stripe = require('stripe')("sk_test_51R46NdFZxzYTUHg2NxBteNzUkTqhwY72yAkklndmY5Qo6QPHqx9TbOBywuDe7zoMhrDhEj62RX9w7evRyIAYxPqW00fei0BlKS");
+// stripe client opzetten
+import Stripe from 'stripe';
+const stripe = new Stripe('sk_test_51R46NdFZxzYTUHg2NxBteNzUkTqhwY72yAkklndmY5Qo6QPHqx9TbOBywuDe7zoMhrDhEj62RX9w7evRyIAYxPqW00fei0BlKS');
 
-// Define storeItems on the server so people can't manipulate the API request
-const storeItems = new Map([
-    // Price needs to be in cents, not in euros
-    [1, {priceInCents: 2000, name: "Mosselen met frietjes"}],
-    [2, {priceInCents: 1500, name: "Videe met frietjes"}],
-    [3, {priceInCents: 1500, name: "Spaghetti"}],
-    [4, {priceInCents: 1200, name: "Frikandel met frietjes"}],
-])
+// payment object aanmaken:
+function createPaymentObject(placedOrder) {
+    return {
+        payment_method_types: ['card', 'bancontact'],
+        mode: 'payment',
+        line_items: placedOrder.dishes.map(dish => {
+            return { 
+                price_data: {
+                    currency: 'EUR',
+                    product_data: {name: dish.name},
+                    unit_amount: dish.unitPrice * 100,
+                },
+                quantity: dish.amount,
+            }
+        }),
+        success_url: 'http://localhost:80',
+        cancel_url: 'http://localhost:80',
+        customer_email: placedOrder.email,
+    }
+}
 
-app.post('/create-checkout-session', async (req, res) => {
+app.get('/payment/start', async (req, res) => {
     try{
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card', 'bancontact'],
-            mode: 'payment',
-            line_items: req.body.items.map(item => {
-                const storeItem = storeItems.get(item.id)
-                return {
-                    price_data: {
-                        currency: 'EUR',
-                        product_data: {
-                            name: storeItem.name,
-                        },
-                        unit_amount: storeItem.priceInCents,
-                    },
-                    quantity: item.quantity,
-                }
-            }),
-            // TODO: change localhost to .env path
-            success_url: 'http://localhost:3000/success.html',
-            cancel_url: 'http://localhost:3000/cancel.html',
-            customer_email: "voornaam.achternaam@gmail.com",
-        });
-        res.json({ url: session.url })
-    }catch(e){
-        res.status(500).json({ error: e.message });
+        const session = await stripe.checkout.sessions.create(createPaymentObject(order));
+        res.redirect(session.url)
+    } catch(e){
+        res.status(500).redirect('http://localhost:80');
     }
 })
 
-app.listen(3000)
+app.listen(port);
